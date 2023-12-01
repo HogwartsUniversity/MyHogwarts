dba61dd86cc92537b5ac10396e15c68f3f8b65e4
prati <prati@Pratik>
2023-12-01 15:13:30 +0530

    Project Structure

---
+++ HogWarts/src/main/java/edu/hogwarts/main/HogWartsApplication.java
@@ -0,0 +1,13 @@
+package edu.hogwarts.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogWartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogWartsApplication.class, args);
+	}
+
+}
